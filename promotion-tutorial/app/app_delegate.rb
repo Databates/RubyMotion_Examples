c44f03225332d6e649053b118663e3ab437190f3
Databates <38bates@gmail.com>
2014-05-18 16:51:15 -0400

    Added more views to show what ProMotion can do

--- promotion-tutorial/app/app_delegate.rb
+++ promotion-tutorial/app/app_delegate.rb
@@ -1,20 +1,11 @@
-class AppDelegate 
-	def application(app, options)
+class AppDelegate < PM::Delegate 
+	
+	def on_load(app, options)  #Takes app and options as its two parameters
+		# In ProMotion we work with screens.
+		# Screens are just a subclass of UIViewController
+		#This basically says, hey when the page loads, go to the Welcome Screen
 		open WelcomeScreen.new(nav_bar: true) 
 	end
 end
-
-
-
-
-
-
-
-# Default AppDelegate
-# class AppDelegate
-#   def application(application, didFinishLaunchingWithOptions:launchOptions)
-#     true
-#   end
-# end
 
 
--- promotion-tutorial/app/screens/welcome_screen.rb
+++ promotion-tutorial/app/screens/welcome_screen.rb
@@ -1,18 +1,23 @@
 class WelcomeScreen < PM::Screen  
+	title "Welcome 2 PM Demo!"
+
 
 	def on_load
-		set_nav_bar_button :left, title: "Help", action: :open_help_screen
-
+		set_nav_bar_button :left, title: "Links", action: :open_links_screen #Good Practice is to write your action names so they describe exactly what they are doing
 	end
 
 	def will_appear #promotions rubyesque way of defining viewWillAppear
 		set_attributes self.view, {
-			background_color: hex_color("#FFFFFF")
+			background_color: hex_color("#FFFFFF") #ProMotion provides some great helpers to make styling easier
 		}
 	end
 
-	def open_help_screen
-		open_modal Help #screen gets presented modally
+	def open_links_screen
+		#open_modal AboutUsScreen.new(nav_bar: true) #screen gets presented modally
+		open_modal LinksScreen.new(nav_bar: true) #screen gets presented modally
+
 	end
 
 end
+
+
